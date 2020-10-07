cde6a0488604566bac8f412255021da08cc41415
Jon Gill <jonpgill1@gmail.com>
2020-10-07 13:35:38 +0000

    products.html:add breadcrumbs, heading and filter buttons

--- products/views.py
+++ products/views.py
@@ -7,8 +7,17 @@ def all_products(request):
     """ A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()
-    context = {
+
+    categories = None
+
+    if 'category' in request.GET:
+        categories = request.GET['category'].split(',')
+        products = products.filter(category__name__in=categories)
+        categories = Category.objects.filter(name__in=categories)
+
+        context = {
         'products': products,
+        'categories': categories,
     }
 
     return render(request, 'products/products.html', context)
