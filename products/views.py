1a70e919401de83e0d2adfec8df00e9afda9c8d0
Jon Gill <jonpgill1@gmail.com>
2020-10-09 09:15:11 +0000

    add product_detail view & template

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product, Category
 
 # Create your views here.
@@ -15,9 +15,21 @@ def all_products(request):
         products = products.filter(category__name__in=categories)
         categories = Category.objects.filter(name__in=categories)
 
-        context = {
+    context = {
         'products': products,
         'categories': categories,
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """ A view to show individual product details """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
