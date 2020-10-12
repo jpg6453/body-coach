7b94d7e0a1d6e19459ea7749c5f58e33d793c5f4
Jon Gill <jonpgill1@gmail.com>
2020-10-12 22:09:08 +0000

    remove secret key and set debug

--- body_coach/settings.py
+++ body_coach/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(+p3#)9nz7k&0toj&djs9c6pnq-kyl81628g+)^$x7y9_pk(%^'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['body-coach.herokuapp.com', 'localhost']
 
